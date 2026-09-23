43d2a26b9459094929cd4af252057c99dd7382cf
Sadiah-Rahman <sadiahchowdhury23@gmail.com>
2026-09-23 10:08:57 +0600

    Add migration to create areas table

--- database/migrations/2026_09_23_035844_create_areas_table.php
+++ database/migrations/2026_09_23_035844_create_areas_table.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('areas',function(Blueprint $table){
+            $table->id();
+            $table->string('name',80)->unique();
+            $table->string('slug',80)->unique();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('areas');
     }
 };
